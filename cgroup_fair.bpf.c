369d1166a12f600b8be47bc24e765b3aad63394f
Ethan Van Der Heijden <ethanvanderheijden@gmail.com>
2025-08-13 17:07:30 -0700

    Tracking vruntime of processes in cgroup and preserve fairness.

--- cgroup_fair.bpf.c
+++ cgroup_fair.bpf.c
@@ -7,6 +7,7 @@ UEI_DEFINE(uei);
 const volatile unsigned long long root_cgroup;  // can be 0 for no restriction
 const volatile int total_cpus;
 
+// FALLBACK_DSQ will simply be FIFO
 #define FALLBACK_DSQ 0
 // slice is 5ms
 #define SLICE 5000000
@@ -38,31 +39,44 @@ struct {
     __uint(max_entries, 1);
 } unallocated_cpus SEC(".maps");
 
-// access is not racy since it is only mutated on cgroup init/exit
+struct cgroup_ctx {
+    // access is not racy since it is only mutated on cgroup init/exit
+    struct bpf_cpumask __kptr *mask;
+    // stores max scx.dsq_vtime for any process in this cgroup that has run
+    // difference between any task->scx.dsq_vtime is the lag time
+    // Warning: make sure to reset lag time in task init and wakeup!
+    u64 max_vtime;
+};
+
 struct {
     __uint(type, BPF_MAP_TYPE_CGRP_STORAGE);
     __uint(map_flags, BPF_F_NO_PREALLOC);
     __type(key, u32);
-    __type(value, struct cpumask_wrapper);
-} cgroup_pinning SEC(".maps");
+    __type(value, struct cgroup_ctx);
+} cgroup_ctx_array SEC(".maps");
 
-// access to assigned_cgroup is racy, but that's OK
-// if a process is placed in the wrong queue, it will get fixed on the next dispatch
+struct cpu_ctx {
+    // access to assigned_cgroup is racy, but that's OK
+    // if a process is placed in the wrong queue, it will get fixed on the next dispatch
+    u32 assigned_cgroup;  // 0 indicates no assigned cgroup
+    // we can't read the `prev` task's cgroup inside dispatch()
+    // so we store whether it has local affinity ahead of time (in running())
+    bool has_local_affinity;
+};
+
 struct {
     __uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
     __type(key, u32);
-    __type(value, u32);  // 0 indicates no assigned cgroup
+    __type(value, struct cpu_ctx);
     __uint(max_entries, 1);
-} assigned_cgroup SEC(".maps");
+} cpu_ctx_array SEC(".maps");
 
-// we can't read the `prev` task's cgroup inside dispatch()
-// so we store whether it has local affinity ahead of time (in running())
 struct {
-    __uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
-    __type(key, u32);
-    __type(value, u32);  // 0 indicates no assigned cgroup
-    __uint(max_entries, 1);
-} has_local_affinity SEC(".maps");
+    __uint(type, BPF_MAP_TYPE_TASK_STORAGE);
+    __uint(map_flags, BPF_F_NO_PREALLOC);
+    __type(key, int);
+    __type(value, u64);
+} task_start_time SEC(".maps");
 
 struct {
     __uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
@@ -81,22 +95,26 @@ static bool is_enabled(void) {
     }
 }
 
-static u32 get_assigned_cgroup(void) {
+static struct cpu_ctx *get_cpu_ctx() {
     u32 key = 0;
-    u32 *val = bpf_map_lookup_elem(&assigned_cgroup, &key);
-    if (!val) {
-        return -1;
-    } else {
-        return *val;
-    }
-}
-
-static struct bpf_cpumask *get_cgroup_cpus(struct cgroup *cgrp) {
-    struct cpumask_wrapper *state = bpf_cgrp_storage_get(&cgroup_pinning, cgrp, NULL, 0);
-    if (state == NULL) {
+    struct cpu_ctx *ctx = bpf_map_lookup_elem(&cpu_ctx_array, &key);
+    if (ctx == NULL) {
+        scx_bpf_error("Failed to get CPU context");
         return NULL;
     }
-    return state->mask;
+    return ctx;
+}
+
+static struct cgroup_ctx *get_cgroup_ctx(struct cgroup *cgrp) {
+    struct cgroup_ctx *ctx = bpf_cgrp_storage_get(&cgroup_ctx_array, cgrp, NULL, 0);
+    if (ctx == NULL) {
+        scx_bpf_error("Failed to get cgroup context for cgroup %llu", cgrp->kn->id);
+        return NULL;
+    }
+    if (ctx->mask == NULL) {
+        scx_bpf_error("Cgroup context for cgroup %llu has no cpumask", cgrp->kn->id);
+    }
+    return ctx;
 }
 
 static struct bpf_cpumask *lock_unallocated_cpus(void) {
@@ -130,14 +148,13 @@ static struct bpf_cpumask *get_valid_cpus(void) {
     return mask;
 }
 
-static u32 *get_local_affinity() {
-    u32 key = 0;
-    u32 *val = bpf_map_lookup_elem(&has_local_affinity, &key);
-    if (!val) {
-        scx_bpf_error("Failed to get local affinity");
+static u64 *get_task_start_time(struct task_struct *p) {
+    u64 *stats = bpf_task_storage_get(&task_start_time, p, 0, 0);
+    if (stats == NULL) {
+        scx_bpf_error("Failed to get task stats for task %d", p->pid);
         return NULL;
     }
-    return val;
+    return stats;
 }
 
 static void stat_inc(u32 idx) {
@@ -148,23 +165,32 @@ static void stat_inc(u32 idx) {
 s32 BPF_STRUCT_OPS(cgroup_fair_select_cpu, struct task_struct *p, s32 prev_cpu, u64 wake_flags) {
     s32 cpu = -1;
     struct cgroup *cgrp = scx_bpf_task_cgroup(p);
-    struct bpf_cpumask *cpus = get_cgroup_cpus(cgrp);
-    if (cpus != NULL) {
-        s32 idle_affinity = scx_bpf_pick_idle_cpu(&cpus->cpumask, 0);
-        if (idle_affinity >= 0) {
-            bpf_cgroup_release(cgrp);
-            scx_bpf_dsq_insert(p, SCX_DSQ_LOCAL, SLICE, 0);
-            bpf_printk("Directly dispatching to idle CPU %d", idle_affinity);
-            stat_inc(2);
-            return idle_affinity;
-        } else {
-            cpu = scx_bpf_pick_any_cpu(&cpus->cpumask, 0);
+    struct cgroup_ctx *cgrp_ctx = get_cgroup_ctx(cgrp);
+    if (cgrp_ctx != NULL) {
+        // task has just woken up
+        // cap lag time to 1 slice
+        if (time_before(p->scx.dsq_vtime, cgrp_ctx->max_vtime - SLICE)) {
+            p->scx.dsq_vtime = cgrp_ctx->max_vtime - SLICE;
+        }
+
+        if (cgrp_ctx->mask != NULL) {
+            struct bpf_cpumask *mask = cgrp_ctx->mask;
+            s32 idle_affinity = scx_bpf_pick_idle_cpu((cpumask_t *)mask, 0);
+            if (idle_affinity >= 0) {
+                bpf_cgroup_release(cgrp);
+                scx_bpf_dsq_insert(p, SCX_DSQ_LOCAL, SLICE, 0);
+                bpf_printk("Directly dispatching to idle CPU %d", idle_affinity);
+                stat_inc(2);
+                return idle_affinity;
+            } else {
+                cpu = scx_bpf_pick_any_cpu((cpumask_t *)mask, 0);
+            }
         }
     }
 
     struct bpf_cpumask *valid_cpus = get_valid_cpus();
     if (valid_cpus != NULL) {
-        s32 idle_foreign = scx_bpf_pick_idle_cpu(&valid_cpus->cpumask, 0);
+        s32 idle_foreign = scx_bpf_pick_idle_cpu((cpumask_t *)valid_cpus, 0);
         if (idle_foreign >= 0) {
             bpf_cgroup_release(cgrp);
             scx_bpf_dsq_insert(p, SCX_DSQ_LOCAL, SLICE, 0);
@@ -173,7 +199,7 @@ s32 BPF_STRUCT_OPS(cgroup_fair_select_cpu, struct task_struct *p, s32 prev_cpu, 
         }
 
         if (cpu == -1) {
-            cpu = scx_bpf_pick_any_cpu(&valid_cpus->cpumask, 0);
+            cpu = scx_bpf_pick_any_cpu((cpumask_t *)valid_cpus, 0);
         }
     }
 
@@ -183,11 +209,13 @@ s32 BPF_STRUCT_OPS(cgroup_fair_select_cpu, struct task_struct *p, s32 prev_cpu, 
 
 void BPF_STRUCT_OPS(cgroup_fair_enqueue, struct task_struct *p, u64 enq_flags) {
     struct cgroup *cgrp = scx_bpf_task_cgroup(p);
-    struct bpf_cpumask *cpus = get_cgroup_cpus(cgrp);
-    if (cpus == NULL) {
+    struct cgroup_ctx *cgrp_ctx = get_cgroup_ctx(cgrp);
+    if (cgrp_ctx == NULL) {
         scx_bpf_dsq_insert(p, FALLBACK_DSQ, SLICE, enq_flags);
     } else {
-        scx_bpf_dsq_insert(p, cgrp->kn->id, SLICE, enq_flags);
+        // bpf_printk("Enqueueing, dif: %lld    max: %llu   proc time: %llu", cgrp_ctx->max_vtime -
+        // p->scx.dsq_vtime, cgrp_ctx->max_vtime, p->scx.dsq_vtime);
+        scx_bpf_dsq_insert_vtime(p, cgrp->kn->id, SLICE, p->scx.dsq_vtime, enq_flags);
     }
     bpf_cgroup_release(cgrp);
 }
@@ -197,18 +225,19 @@ void BPF_STRUCT_OPS(cgroup_fair_dispatch, s32 cpu, struct task_struct *prev) {
         return;
     }
 
-    u32 assigned_cgrp = get_assigned_cgroup();
-    if (assigned_cgrp > 0) {
-        if (scx_bpf_dsq_move_to_local(assigned_cgrp)) {
-            // successfully switched out `prev` with a local-affinity process
-            stat_inc(0);
-            return;
-        } else if (prev != NULL) {
-            u32 *local_affinity = get_local_affinity();
-            if (local_affinity != NULL && *local_affinity) {
-                // `prev` is a local-affinity process, let it keep running
-                stat_inc(1);
+    struct cpu_ctx *curr_ctx = get_cpu_ctx();
+    if (curr_ctx != NULL) {
+        if (curr_ctx->assigned_cgroup > 0) {
+            if (scx_bpf_dsq_move_to_local(curr_ctx->assigned_cgroup)) {
+                // successfully switched out `prev` with a local-affinity process
+                stat_inc(0);
                 return;
+            } else if (prev != NULL) {
+                if (curr_ctx->has_local_affinity) {
+                    // `prev` is a local-affinity process, let it keep running
+                    stat_inc(1);
+                    return;
+                }
             }
         }
     }
@@ -218,22 +247,48 @@ void BPF_STRUCT_OPS(cgroup_fair_dispatch, s32 cpu, struct task_struct *prev) {
 }
 
 void BPF_STRUCT_OPS(cgroup_fair_running, struct task_struct *p) {
-    u32 *local_affinity = get_local_affinity();
-    u32 assigned_cgrp = get_assigned_cgroup();
-    if (local_affinity != NULL && assigned_cgrp > 0) {
-        struct cgroup *cgrp = scx_bpf_task_cgroup(p);
-        *local_affinity = cgrp->kn->id == assigned_cgrp;
+    struct cgroup *cgrp = scx_bpf_task_cgroup(p);
+
+    struct cpu_ctx *curr_ctx = get_cpu_ctx();
+    if (curr_ctx != NULL) {
+        if (curr_ctx->assigned_cgroup > 0) {
+            curr_ctx->has_local_affinity = cgrp->kn->id == curr_ctx->assigned_cgroup;
+        }
     }
 
-    // if (time_before(vtime_now, p->scx.dsq_vtime)) vtime_now = p->scx.dsq_vtime;
+    struct cgroup_ctx *cgrp_ctx = get_cgroup_ctx(cgrp);
+    if (cgrp_ctx != NULL) {
+        // updating max_vtime is racy! but let's just live with it
+        if (time_before(cgrp_ctx->max_vtime, p->scx.dsq_vtime)) {
+            cgrp_ctx->max_vtime = p->scx.dsq_vtime;
+        }
+    }
+
+    u64 *start = get_task_start_time(p);
+    if (start != NULL) {
+        *start = scx_bpf_now();
+    }
+
+    bpf_cgroup_release(cgrp);
 }
 
 void BPF_STRUCT_OPS(cgroup_fair_stopping, struct task_struct *p, bool runnable) {
-    // p->scx.dsq_vtime += (SCX_SLICE_DFL - p->scx.slice) * 100 / p->scx.weight;
+    u64 *start = get_task_start_time(p);
+    if (start != NULL) {
+        u64 now = scx_bpf_now();
+        p->scx.dsq_vtime += now - *start;
+    }
 }
 
-void BPF_STRUCT_OPS(cgroup_fair_enable, struct task_struct *p) {
-    // p->scx.dsq_vtime = vtime_now;
+s32 BPF_STRUCT_OPS(cgroup_fair_init_task, struct task_struct *p, struct scx_init_task_args *args) {
+    bpf_task_storage_get(&task_start_time, p, 0, BPF_LOCAL_STORAGE_GET_F_CREATE);
+
+    // this may be called before cgroup_fair_cgroup_init()
+    struct cgroup_ctx *cgrp_ctx = bpf_cgrp_storage_get(&cgroup_ctx_array, args->cgroup, NULL, 0);
+    if (cgrp_ctx != NULL) {
+        p->scx.dsq_vtime = cgrp_ctx->max_vtime;
+    }
+    return 0;
 }
 
 s32 BPF_STRUCT_OPS_SLEEPABLE(cgroup_fair_cgroup_init, struct cgroup *cgrp,
@@ -256,8 +311,8 @@ s32 BPF_STRUCT_OPS_SLEEPABLE(cgroup_fair_cgroup_init, struct cgroup *cgrp,
 
     bpf_printk("Initializing child cgroup %llu", root_cgroup, cgrp->kn->id);
 
-    struct cpumask_wrapper *state =
-        bpf_cgrp_storage_get(&cgroup_pinning, cgrp, NULL, BPF_LOCAL_STORAGE_GET_F_CREATE);
+    struct cgroup_ctx *state =
+        bpf_cgrp_storage_get(&cgroup_ctx_array, cgrp, NULL, BPF_LOCAL_STORAGE_GET_F_CREATE);
     if (state == NULL) {
         scx_bpf_error("Failed to create cgroup state for %llu", cgrp->kn->id);
         ret = -ENOMEM;
@@ -277,7 +332,7 @@ s32 BPF_STRUCT_OPS_SLEEPABLE(cgroup_fair_cgroup_init, struct cgroup *cgrp,
         goto unlock;
     }
 
-    if (bpf_cpumask_weight(&cpumask->cpumask) < cpus_needed) {
+    if (bpf_cpumask_weight((cpumask_t *)cpumask) < cpus_needed) {
         scx_bpf_error("Not enough cpus available for new cgroup");
         ret = -ENOSPC;
         goto unlock;
@@ -295,13 +350,13 @@ s32 BPF_STRUCT_OPS_SLEEPABLE(cgroup_fair_cgroup_init, struct cgroup *cgrp,
     u32 key = 0;
     int i;
     bpf_for(i, 0, cpus_needed) {
-        int i = bpf_cpumask_first(&cpumask->cpumask);
+        int i = bpf_cpumask_first((cpumask_t *)cpumask);
         bpf_cpumask_set_cpu(i, soft_pin);
         bpf_cpumask_clear_cpu(i, cpumask);
 
-        u32 *assigned_cgrp = bpf_map_lookup_percpu_elem(&assigned_cgroup, &key, i);
-        if (assigned_cgrp != NULL) {
-            *assigned_cgrp = cgrp->kn->id;
+        struct cpu_ctx *ctx = bpf_map_lookup_percpu_elem(&cpu_ctx_array, &key, i);
+        if (ctx != NULL) {
+            ctx->assigned_cgroup = cgrp->kn->id;
         } else {
             scx_bpf_error("Failed to set assigned cgroup for CPU %d", i);
         }
@@ -319,27 +374,27 @@ done:
 }
 
 void BPF_STRUCT_OPS_SLEEPABLE(cgroup_fair_cgroup_exit, struct cgroup *cgrp) {
-    struct cpumask_wrapper *state = bpf_cgrp_storage_get(&cgroup_pinning, cgrp, NULL, 0);
+    struct cgroup_ctx *state = get_cgroup_ctx(cgrp);
     if (state != NULL) {
         scx_bpf_destroy_dsq(cgrp->kn->id);
         struct bpf_cpumask *soft_pinning = bpf_kptr_xchg(&state->mask, NULL);
         if (soft_pinning != NULL) {
             struct bpf_cpumask *unallocated_mask = lock_unallocated_cpus();
             if (unallocated_mask != NULL) {
-                bpf_cpumask_or(unallocated_mask, &unallocated_mask->cpumask,
-                               &soft_pinning->cpumask);
+                bpf_cpumask_or(unallocated_mask, (cpumask_t *)unallocated_mask,
+                               (cpumask_t *)soft_pinning);
             }
             bpf_rcu_read_unlock();
 
             int i;
-            bpf_for(i, 0, bpf_cpumask_weight(&soft_pinning->cpumask)) {
-                u32 cpu = bpf_cpumask_first(&soft_pinning->cpumask);
+            bpf_for(i, 0, bpf_cpumask_weight((cpumask_t *)soft_pinning)) {
+                u32 cpu = bpf_cpumask_first((cpumask_t *)soft_pinning);
                 bpf_cpumask_clear_cpu(cpu, soft_pinning);
 
                 u32 key = 0;
-                u32 *assigned_cgrp = bpf_map_lookup_percpu_elem(&assigned_cgroup, &key, cpu);
-                if (assigned_cgrp != NULL) {
-                    *assigned_cgrp = 0;
+                struct cpu_ctx *ctx = bpf_map_lookup_percpu_elem(&cpu_ctx_array, &key, cpu);
+                if (ctx != NULL) {
+                    ctx->assigned_cgroup = 0;
                 } else {
                     scx_bpf_error("Failed to clear assigned cgroup for CPU %d", cpu);
                 }
@@ -372,9 +427,9 @@ s32 BPF_STRUCT_OPS_SLEEPABLE(cgroup_fair_init) {
             bpf_cpumask_set_cpu(i, new_mask);
         }
     }
-    bpf_cpumask_copy(new_mask2, &new_mask->cpumask);
+    bpf_cpumask_copy(new_mask2, (cpumask_t *)new_mask);
 
-    bpf_printk("Total CPUs enabled: %d", bpf_cpumask_weight(&new_mask->cpumask));
+    bpf_printk("Total CPUs enabled: %d", bpf_cpumask_weight((cpumask_t *)new_mask));
 
     int key = 0;
     struct cpumask_wrapper *valid = bpf_map_lookup_elem(&valid_cpus, &key);
@@ -412,7 +467,7 @@ struct sched_ext_ops sched_ops = {.select_cpu = (void *)cgroup_fair_select_cpu,
                                   .dispatch = (void *)cgroup_fair_dispatch,
                                   .running = (void *)cgroup_fair_running,
                                   .stopping = (void *)cgroup_fair_stopping,
-                                  .enable = (void *)cgroup_fair_enable,
+                                  .init_task = (void *)cgroup_fair_init_task,
                                   .cgroup_init = (void *)cgroup_fair_cgroup_init,
                                   .cgroup_exit = (void *)cgroup_fair_cgroup_exit,
                                   .init = (void *)cgroup_fair_init,
